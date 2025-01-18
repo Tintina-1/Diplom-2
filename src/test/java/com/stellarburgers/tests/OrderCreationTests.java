57bf0a05b691cd62ae421481273b8e0c6d3f4178
Tintina-1 <tintina.adv@yandex.com>
2025-01-18 19:42:59 +0300

    Added order creation tests: - successfully create an order with authorisation - successfully create an order without authorisation (expected behaviour) - successfully create an order with authorisation with valid ingredients -unsuccessful order creation without ingredients -unsuccessful order creation with incorrect ingredient hash

---
+++ src/test/java/com/stellarburgers/tests/OrderCreationTests.java
@@ -0,0 +1,87 @@
+package com.stellarburgers.tests;
+
+import io.qameta.allure.Step;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class OrderCreationTests extends BaseTest {
+
+    @Test
+    @Step("Тест: создание заказа с авторизацией")
+    public void createOrderWithAuthorizationSuccess() {
+        String[] ingredients = {"61c0c5a71d1f82001bdaaa6d","61c0c5a71d1f82001bdaaa6f"};
+
+        Response response = RestAssured.given()
+                .header("Content-Type", "application/json")
+                .header("Authorization", authToken)
+                .body(String.format("{\"ingredients\": [\"%s\", \"%s\"]}", ingredients[0], ingredients[1]))
+                .post("/orders");
+
+        response.then().statusCode(200)
+                .body("success", equalTo(true))
+                .body("order.number", notNullValue());
+    }
+
+    @Test
+    @Step("Тест: создание заказа без авторизации разрешено")
+    public void createOrderWithoutAuthorizationSuccess() {
+        String[] ingredients = {"61c0c5a71d1f82001bdaaa6d","61c0c5a71d1f82001bdaaa6f"};
+
+        Response response = RestAssured.given()
+                .header("Content-Type", "application/json")
+                .body(String.format("{\"ingredients\": [\"%s\", \"%s\"]}", ingredients[0], ingredients[1]))
+                .post("/orders");
+
+        response.then().statusCode(200)
+                .body("success", equalTo(true))
+                .body("order.number", notNullValue());
+    }
+
+    @Test
+    @Step("Тест: создание заказа с корректными ингредиентами")
+    public void createOrderWithValidIngredientsSuccess() {
+        String[] ingredients = {"61c0c5a71d1f82001bdaaa70", "61c0c5a71d1f82001bdaaa6c"};
+
+        Response response = RestAssured.given()
+                .header("Content-Type", "application/json")
+                .header("Authorization", authToken)
+                .body(String.format("{\"ingredients\": [\"%s\", \"%s\"]}", ingredients[0], ingredients[1]))
+                .post("/orders");
+
+        response.then().statusCode(200)
+                .body("success", equalTo(true))
+                .body("order.number", notNullValue());
+    }
+
+    @Test
+    @Step("Тест: попытка создания заказа без ингредиентов")
+    public void createOrderWithoutIngredientsFailure() {
+        Response response = RestAssured.given()
+                .header("Content-Type", "application/json")
+                .header("Authorization", authToken)
+                .body("{\"ingredients\": []}")
+                .post("/orders");
+
+        response.then().statusCode(400)
+                .body("success", equalTo(false))
+                .body("message", equalTo("Ingredient ids must be provided"));
+    }
+
+    @Test
+    @Step("Тест: попытка создания заказа с неверным хэшем ингредиентов")
+    public void createOrderWithInvalidIngredientHashFailure() {
+        String[] ingredients = {"61c0c5a71d1f82001bdaaa702311"};
+
+        Response response = RestAssured.given()
+                .header("Content-Type", "application/json")
+                .header("Authorization", authToken)
+                .body(String.format("{\"ingredients\": [\"%s\"]}", ingredients[0]))
+                .post("/orders");
+
+        response.then().statusCode(500);
+    }
+}
